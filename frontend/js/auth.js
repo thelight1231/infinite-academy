7e417165d417aded36eb5a597b1e1be539472387
thelight1231 <bondokmolto@gmail.com>
2025-01-30 13:44:04 +0200

    Update fetch requests to include credentials

--- frontend/js/auth.js
+++ frontend/js/auth.js
@@ -14,6 +14,7 @@ const auth = {
                 headers: {
                     'Content-Type': 'application/json',
                 },
+                credentials: 'include',
                 body: JSON.stringify({ email, password })
             });
 
@@ -47,6 +48,7 @@ const auth = {
                 headers: {
                     'Content-Type': 'application/json',
                 },
+                credentials: 'include',
                 body: JSON.stringify({ name, email, password })
             });
 
